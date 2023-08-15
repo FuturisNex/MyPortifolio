94494e7a31b5cde0f4f45fc9c881a3d51396aee9
Gustavo <gugabds0@gmail.com>
2023-08-15 07:25:39 -0300

    Atualização de Projetos e Skill

--- src/componentes/sections/Projects.js
+++ src/componentes/sections/Projects.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ButtonB from '../elements/ButtonB';
 import styles from './styles/Projects.module.css';
 import Card from '../elements/Card';
 import card1 from '../../images/projects/card1.jpeg';
 import card2 from '../../images/projects/card2.jpeg';
+import card3 from '../../images/projects/card3.jpeg';
 
 function Projects() {
     return (
@@ -13,18 +13,23 @@ function Projects() {
                 <Card
                     img={card1}
                     title="Sistema De Dashboard"
-                    tech="HTML, CSS, JS"
+                    tech="HTML, CSS, JS, REACT"
                     description="Desenvolvimento de um dashboard para o setor comercial."
                     repo="https://github.com/Gustavo-LionStar/Sao-Roque.git"
-                    site="https://saoroque-compras.vercel.app/"
                 />
                 <Card
                     img={card2}
                     title="Portfólio"
-                    tech="HTML, CSS, JS"
+                    tech="HTML, CSS, JS, REACT"
                     description="Este é o repositório deste projeto do Portfólio."
                     repo="https://github.com/Gustavo-LionStar/MyPortifolio.git"
-                    site="https://my-portifolio-lionstar.vercel.app"
+                />
+                <Card
+                    img={card3}
+                    title="Formulário"
+                    tech="HTML, CSS, JS, REACT"
+                    description="Este é o repositório de um formulário integrado com o google AppScript."
+                    repo="https://github.com/Gustavo-LionStar/comprador.git"
                 />
             </div>
         </div>
