940a94e0fb38b2d7239e12929dfabe7b19d68de3
Gustavo <gugabds0@gmail.com>
2023-08-14 23:40:45 -0300

    Atualização
    Cards, correção

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,39 +1,22 @@
-import styles from '../elements/styles/Card.module.css'
-import ButtonB from './ButtonB'
-import {useState} from 'react'
+import styles from '../elements/styles/Card.module.css';
+import ButtonB from './ButtonB';
 
-
-function Card ({img, title, tech, description, repo, site }){
-
-const [info, setInfo] = useState(false)
-
-
-function infoOn(){
-    setInfo(true)
+function Card({ img, title, tech, description, repo, site }) {
+  return (
+    <div className={styles.Card}>
+      <a href={site}>
+        <img src={img} alt='ERRO' />
+      </a>
+      <div className={styles.CardInfo}>
+        <h3> {title} </h3>
+        <p>
+          <strong> Tecnologia:</strong> {tech}
+        </p>
+        <p> {description} </p>
+        <ButtonB text='Acesse o repositório' link={repo} />
+      </div>
+    </div>
+  );
 }
 
-function infoOff(){
-    setInfo(false)
-}
-
-    return(
-        <div onMouseLeave={infoOff} className={styles.Card}>
-            <a onMouseEnter={infoOn} href={site}>
-                <img src={img} alt='ERRO'/>
-            </a>
-
-            {info === true &&(
-                <section>
-                <h3> {title} </h3>
-                <p><strong> Tecnologia:</strong> {tech} </p>
-                <p> {description} </p>
-                <ButtonB text='Acesse o repositório' link={repo} />
-            </section>
-            )}
-            
-            
-            </div>
-    )
-}
-
-export default Card
+export default Card;
--- src/componentes/sections/Projects.js
+++ src/componentes/sections/Projects.js
@@ -1,33 +1,34 @@
-import ButtonB from '../elements/ButtonB'
-import styles from './styles/Projects.module.css'
-import Card from '../elements/Card' 
-import card1 from '../../images/projects/card1.jpeg'
-import card2 from '../../images/projects/card2.jpeg'
+import React from 'react';
+import ButtonB from '../elements/ButtonB';
+import styles from './styles/Projects.module.css';
+import Card from '../elements/Card';
+import card1 from '../../images/projects/card1.jpeg';
+import card2 from '../../images/projects/card2.jpeg';
 
-function Projects (){
+function Projects() {
     return (
         <div className={styles.Projects} id='Projects'>
             <h1>Projetos</h1>
-            <Card
-            img={card1} 
-            title="Sistema São Roque" 
-            tech="HTML, CSS, JS"
-            description="Desenvilvimento de  um dashboard para o setor comercial."
-            repo="https://github.com/Gustavo-LionStar/Sao-Roque.git" 
-            site="https://saoroque-compras.vercel.app/"
-            />
-            <Card
-            img={card2} 
-            title="Portifolio" 
-            tech="HTML, CSS, JS"
-            description="Este é o repositorio deste projeto do Portifolio."
-            repo="https://github.com/Gustavo-LionStar/MyPortifolio.git" 
-            site="my-portifolio-lionstar.vercel.app"
-            />
+            <div className={styles.cardContainer}>
+                <Card
+                    img={card1}
+                    title="Sistema De Dashboard"
+                    tech="HTML, CSS, JS"
+                    description="Desenvolvimento de um dashboard para o setor comercial."
+                    repo="https://github.com/Gustavo-LionStar/Sao-Roque.git"
+                    site="https://saoroque-compras.vercel.app/"
+                />
+                <Card
+                    img={card2}
+                    title="Portfólio"
+                    tech="HTML, CSS, JS"
+                    description="Este é o repositório deste projeto do Portfólio."
+                    repo="https://github.com/Gustavo-LionStar/MyPortifolio.git"
+                    site="https://my-portifolio-lionstar.vercel.app"
+                />
             </div>
-            
-    )
-        
+        </div>
+    );
 }
 
-export default Projects
+export default Projects;
